2468f3c178784a2956d914fd48909917958265ef
jaum1502 <aquelecaraalto@gmail.com>
2024-05-21 15:08:25 -0300

    so nos compiuter filhao

--- src/App3.java
+++ src/App3.java
@@ -5,12 +5,12 @@ public class App3 {
         Scanner entrada = new Scanner(System.in);
 
         double[] vet = new double[12];
-        for (int i = 0; i < 12; i++) {
+        for (int i = 0; i < vet.length; i++) {
             System.out.println("Digite o valor " + (i + 1) + ": ");
             vet[i] = entrada.nextFloat();
         }
 
-        for (int i = 0; i < 12; i++) {
+        for (int i = 0; i < vet.length; i++) {
             if (vet[i] % 2 == 0) {
                 vet[i] = vet[i] + (vet[i] * 0.02);
                 System.out.println(vet[i]);
