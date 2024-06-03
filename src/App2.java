6102f3e26d4db1dba370a794c06e90a781125e8a
jaum1502 <aquelecaraalto@gmail.com>
2024-06-03 17:54:09 -0300

    so to no segundo exercicio, mudei de ideia e vou me matar hoje as 22h34

--- src/App2.java
+++ src/App2.java
@@ -1,50 +1,49 @@
 import java.util.Scanner;
 
 public class App2 {
-    public App2() {
-        Scanner s = new Scanner(System.in);
-        float[] vet = new float[12];
-        for (int i = 0; i < 12; i++) {
-            System.out.println("Valor " + (i + 1) + ": ");
-            vet[i] = s.nextFloat();
-        }
-        System.out.println("V - Informa os valores;");
-        System.out.println("M - Informa a média;");
-        System.out.println("T - Informa os valores maiores que a média ");
-        char pergunta = s.next().toLowerCase().charAt(0);
-        float media = 0;
-        if (pergunta == 'v') {
-            for (int i = 0; i < 12; i++) {
-                System.out.println("VETOR " + i + ": " + vet[i]);
-            }
-        }
-        
-        else if(pergunta=='m'){
-            float numero = 0;
-            for (int i = 0; i < 12; i++) {
-            numero = numero+vet[i];
-            }
-            media = numero/vet.length;
-            System.out.println("MÉDIA: "+media);
-        }
-        
-else if (pergunta=='t') {
-    for (int i = 0; i < 12; i++){
-        double teste = vet[i];
-        if(teste>media){
-            System.out.println(vet[i]);
-        }else{System.out.println("no");}
-
-
-
-
-    }
-
-
-
+public App2(){
+Scanner entrada = new Scanner(System.in);
+int tamanho=10;
+float[] vet = new float[tamanho];
+vet=escreverValores(entrada, vet, tamanho);
+calculoMedia(vet, tamanho);
+maiorMedia(vet, tamanho, tamanho);
 }
-s.close();
+
+public float[] escreverValores(Scanner entrada,float vet[], float tamanho){
+for(int i=0; i<tamanho;i++){
+    System.out.print("Vetor [" + (i) + "] - ");
+    vet[i]=entrada.nextFloat();
+}
+    return vet;
+}
+
+
+public void calculoMedia(float vet[],float tamanho){
+float soma=0;
+for(int i=0; i<tamanho;i++){
+    soma+=vet[i];
+}
+ float media=soma/tamanho;
+ System.out.println("----------------------");
+ System.out.println("MÉDIA: "+media);
+ System.out.println("----------------------");
+}
+
+public void maiorMedia(float vet[], float media, int tamanho){
+    System.out.println("MAIORES QUE A MÉDIA:");
+for(int i=0; i<tamanho;i++ ){
+    if (vet[i]>media) {
+    System.out.println("VETOR " + i + " - "+vet[i]);
     }
+    
+}
+System.out.println("----------------------");
+}
+
+
+
+
 
     public static void main(String[] args) {
         new App2();
